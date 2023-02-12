5ac96269b4422e2dd0d0e62f59943dbc29e56995
阿洋 <xinzhuxiansheng@126.com>
2023-02-13 01:15:35 +0800

    添加user模块枚举值

---
+++ common/src/main/java/com/yzhou/im/common/enums/ConversationErrorCode.java
@@ -0,0 +1,28 @@
+package com.yzhou.im.common.enums;
+
+
+import com.yzhou.im.common.exception.ApplicationExceptionEnum;
+
+public enum ConversationErrorCode implements ApplicationExceptionEnum {
+
+    CONVERSATION_UPDATE_PARAM_ERROR(50000,"會話修改參數錯誤"),
+
+
+    ;
+
+    private int code;
+    private String error;
+
+    ConversationErrorCode(int code, String error){
+        this.code = code;
+        this.error = error;
+    }
+    public int getCode() {
+        return this.code;
+    }
+
+    public String getError() {
+        return this.error;
+    }
+
+}
